f5cba0118a0d13d1e8052cd5a4debd4986a2418b
angelddeleon <angeldeleonpr00@gmail.com>
2025-04-17 16:47:37 -0400

    configurado a la base de datos del cpanel

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'mi_clave_secreta'
     
     # Configuración de la base de datos (MySQL con PyMySQL)
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = '#Lo28de06'
+    MYSQL_USER = 'waikikip_admin'
+    MYSQL_PASSWORD = 'K(ivE][]T1{b'
     MYSQL_HOST = 'localhost'
     MYSQL_PORT = 3306
-    MYSQL_DB = 'waikiki'
+    MYSQL_DB = 'waikikip_waikiki'
     
     # Cadena de conexión modificada para usar PyMySQL
     SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}'
